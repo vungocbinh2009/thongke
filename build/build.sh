b61e58ac57634c55470c21a7a997bd4e330f6728
binh <vungocbinh@protonmail.com>
2021-11-14 15:09:38 +0700

    add data_* function to simulate data

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
 cd ..
-cat src/util.R src/estimate.R src/hypothesis_check.R src/hypothesis_test.R src/regression.R > R/thongke.R
+cat src/util.R src/estimate.R src/hypothesis_check.R src/hypothesis_test.R src/regression.R src/data.R > R/thongke.R
 Rscript build/build.R
 
---
+++ src/data.R
@@ -0,0 +1,65 @@
+#' Hàm này tạo dữ liệu giả theo phân bố chuẩn từ min đến max
+#' Hàm sẽ in ra bảng phân bố tần số
+#' Lưu ý: dữ liệu chỉ cho giá trị mean và sd gần đúng và không bằng giá trị mean và sd truyền vào hàm
+#' @export
+#' @importFrom truncnorm rtruncnorm
+data_simulate_discrete <- function(n, mean, sd, min, max) {
+  data <- rtruncnorm(n, a=min, b=max, mean, sd)
+  data <- round(data)
+  df <- data.frame(data1 = data)
+  print(table(df$data1))
+  return(data)
+}
+
+#' Hàm này tạo dữ liệu giả theo phân bố chuẩn từ min đến max
+#' Hàm sẽ in ra bảng phân bố tần số ghép lớp, theo các giá trị cut cho trước
+#' Lưu ý: dữ liệu chỉ cho giá trị mean và sd gần đúng và không bằng giá trị mean và sd truyền vào hàm
+#' @export
+#' @importFrom truncnorm rtruncnorm
+data_simulate_continuous <- function(n, mean, sd, min, max, size) {
+  data <- rtruncnorm(n, a=min, b=max, mean, sd)
+  df <- data.frame(data = data)
+  cut_vector <- get_cut_vector(min, max, size)
+  df$data.cut <- cut(df$data, breaks=cut_vector)
+  print(with(df, table(data.cut, useNA='ifany')))
+
+  df$cal_data <- as.numeric(df$data.cut) * size + (min - size / 2)
+  return(df$cal_data)
+}
+
+#' Hàm này tạo dữ liệu giả để xây dựng bài toán hồi quy tuyến tính đơn
+#' Lưu ý: Các hệ số hồi quy tuyến tính truyền vào không phải các hệ số hồi quy tuyến tính cuối cùng
+#' Hàm này return 2 giá trị, có thể nhận 2 giá trị này thông qua lệnh
+#' library(gsubfn)
+#' list[x, y] <- simulate_regression_data(...)
+#' @export
+data_simulate_regression <- function(n, min_x, max_x, b0, b1, sd_eps, round_digits) {
+  # Tạo ngẫu nhiên dữ liệu cho các biến x_1, x_2
+  x <- runif(n, min_x, max_x)
+
+  # Tạo các tham số b0, b1, b2 cũng như sai số eps (theo phân bố chuẩn với sigma cố định)
+  b0 <- b0
+  b1 <- b1
+
+  eps <- rnorm(n, mean = 0, sd = sd_eps)
+  y <- b0 + b1 * x + eps
+
+  x <- round(x, digits = round_digits)
+  y <- round(y, digits = round_digits)
+
+  df <- data.frame(x = x, y = y)
+  df <- df[order(x),]
+  print(df)
+  return(list(x = x, y = y))
+}
+
+#' Hàm này xây dựng cut_vector cho hàm data_simulate_regression
+get_cut_vector <- function(min, max, size) {
+  groups <- (max - min) / size
+  cut_vector <- min
+  for(i in 1:groups) {
+    cut_vector <- c(cut_vector, min + i*size)
+  }
+  return(cut_vector)
+}
+
