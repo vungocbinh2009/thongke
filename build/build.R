8590c349912435711591625d3939a1172383d582
binh <vungocbinh@protonmail.com>
2021-11-25 21:50:38 +0700

    Change build.R script to merge files in R

--- build/build.R
+++ build/build.R
@@ -1,6 +1,8 @@
 library(devtools)
 library(here)
+library(magrittr)
 
+# This function merge all text file to 1 file
 merge_files <- function(file_list, output) {
   output_file <- NULL
   for(file in file_list) {
@@ -11,7 +13,8 @@ merge_files <- function(file_list, output) {
 }
 
 ### BEGIN SCRIPT
-file_list <- here("src", list.files(here("src")))
+# Thực hiện các bước: Tìm folder src, list các file, rồi lấy đường dẫn các file này.
+file_list <- here("src") %>% list.files() %>% here("src", .)
 
 merge_files(file_list, here("R", "thongke.R"))
 
