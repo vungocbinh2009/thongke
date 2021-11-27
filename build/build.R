7627e96420501b09cd92061117d70143c747387d
binh <vungocbinh@protonmail.com>
2021-11-27 19:58:43 +0700

    copy code from src to R to remove src.

--- build/build.R
+++ build/build.R
@@ -1,22 +1,4 @@
 library(devtools)
-library(here)
-library(magrittr)
-
-# This function merge all text file to 1 file
-merge_files <- function(file_list, output) {
-  output_file <- NULL
-  for(file in file_list) {
-    output_file <- c(output_file, readLines(file))
-    output_file <- c(output_file, "\n")
-  }
-  writeLines(output_file, output)
-}
-
-### BEGIN SCRIPT
-# Thực hiện các bước: Tìm folder src, list các file, rồi lấy đường dẫn các file này.
-file_list <- here("src") %>% list.files() %>% here("src", .)
-
-merge_files(file_list, here("R", "thongke.R"))
 
 build(
   pkg = ".",
@@ -30,7 +12,6 @@ build(
 
 document(pkg = ".", roclets = NULL, quiet = FALSE)
 
-### END SCRIPT
 
 
 
